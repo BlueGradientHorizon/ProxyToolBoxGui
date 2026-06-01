7f7243fe1c85153c274e3f462af344110f557a4a
BlueGradientHorizon <safana53656@gmail.com>
2026-06-01 22:50:48 +0300

    Version 1.1.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.bghorizon.proxytoolboxgui"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = appVersion
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
